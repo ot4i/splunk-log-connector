e05de262bdb7b72872957d3cb692b2d4b7158e05
carsten <carsten.bornert@uk.ibm.com>
2015-08-19 08:25:39 +0100

    added copyright statement

--- src/SplunkLogConnector/src/test/com/ibm/connectors/splunklog/SplunkLogOutputConnectorTest.java
+++ src/SplunkLogConnector/src/test/com/ibm/connectors/splunklog/SplunkLogOutputConnectorTest.java
@@ -1,3 +1,22 @@
+/*
+
+Copyright 2015 IBM Corporation
+Author: Carsten Börnert
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 package com.ibm.connectors.splunklog;
 
 import static org.junit.Assert.fail;
@@ -44,15 +63,12 @@ public class SplunkLogOutputConnectorTest {
 			fail("unable to get outputinteraction");
 		} else {
 			Properties props = new Properties();
-			//props.setProperty("host", "prd-p-jbdc68k7lzg5.cloud.splunk.com");
 			props.setProperty("host", "192.168.56.1");
 			props.setProperty("resource", "/services/receivers/simple");
 			props.setProperty("usehttps", "yes");
 			props.setProperty("splunk_UT", "yes!");
 			props.setProperty("splunk_TestParam2", "lkadskjadkjsfjkldsjf");
 //			
-//			props.setProperty("user", "cb333");
-//			props.setProperty("pass", "cbt3ster");
 			
 			props.setProperty("user", "admin");
 			props.setProperty("pass", "tester");
